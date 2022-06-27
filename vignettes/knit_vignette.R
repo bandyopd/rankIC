68ec31ec14c81b07ff449808f76ca3c677f21299
Taehwa Choi <80399859+taehwa015@users.noreply.github.com>
2022-06-27 10:37:28 +0900

    Delete knit_vignette.R

--- vignettes/knit_vignette.R
+++
@@ -1,9 +0,0 @@
-library(knitr)
-# remove file path vignettes
-setwd("/Users/thchoi/Library/Mobile Documents/com~apple~CloudDocs/Drive/Packages/rankIC/")
-replace = readLines("vignettes/rankIC.Rmd")
-replace = gsub("<img src=\"vignettes/", "<img src=\"", replace)
-fileConn = file("vignettes/rankIC.Rmd")
-writeLines(replace, fileConn)
-close(fileConn)
-
